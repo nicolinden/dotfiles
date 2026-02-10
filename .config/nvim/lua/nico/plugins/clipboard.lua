c2fb465cd7ed057ecf7f075ed84e1c0e6c3c0557
Nico van der Linden <nicolinden@gmail.com>
2026-02-10 12:13:03 +0100

    clipboard fix

--- .config/nvim/lua/nico/plugins/clipboard.lua
+++ .config/nvim/lua/nico/plugins/clipboard.lua
@@ -1,17 +1,43 @@
+-- Linux over SSH -> macOS clipboard via OSC52
+-- Op macOS zelf doen we niks (native clipboard).
+
 if vim.fn.has("unix") == 1 and vim.fn.has("macunix") == 0 then
-  -- alleen op Linux (dus niet op macOS)
   return {
     {
       "ojroques/nvim-osc52",
       config = function()
-        require("osc52").setup()
-        vim.keymap.set("n", "<leader>y", require("osc52").copy_operator, { expr = true })
+        local osc52 = require("osc52")
+
+        osc52.setup({
+          max_length = 0,
+          silent = true,
+          trim = false,
+        })
+
+        local function copy(lines, _)
+          -- Neovim geeft {lines}; osc52 wil string
+          osc52.copy(table.concat(lines, "\n"))
+        end
+
+        local function paste()
+          -- “paste via osc52” kan niet echt; pak gewoon unnamed register
+          local content = vim.fn.getreg('"')
+          return vim.split(content, "\n"), vim.fn.getregtype('"')
+        end
+
+        vim.g.clipboard = {
+          name = "osc52",
+          copy = { ["+"] = copy, ["*"] = copy },
+          paste = { ["+"] = paste, ["*"] = paste },
+        }
+
+        -- Optioneel: behoud je leader mappings
+        vim.keymap.set("n", "<leader>y", osc52.copy_operator, { expr = true })
         vim.keymap.set("n", "<leader>Y", "<leader>y_", { remap = true })
-        vim.keymap.set("v", "<leader>y", require("osc52").copy_visual)
+        vim.keymap.set("v", "<leader>y", osc52.copy_visual)
       end,
     },
   }
 end
 
--- op macOS (of andere gevallen) geen clipboard-plugin nodig:
 return {}
