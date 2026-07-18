be09bccc1ca97fb38460cd5a1631311dc21a2b66
Nico van der Linden <nicolinden@gmail.com>
2026-07-18 15:55:43 +0200

    small fixes

---
+++ home/.config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- Gebruik twee spaties voor inspringing
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
+-- Gebruik het macOS-klembord
+vim.opt.clipboard = "unnamedplus"
--- home/.zshrc
+++ home/.zshrc
@@ -1,2 +1,7 @@
 PATH="$PATH:/Applications/WezTerm.app/Contents/MacOS"
 export PATH
+
+# prompt
+if command -v starship >/dev/null 2>&1; then
+	eval "$(starship init zsh)"
+fi
