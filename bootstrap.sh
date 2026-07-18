ab9967e92708e11707707028ea6472b40a514ebd
Nico van der Linden <nicolinden@gmail.com>
2026-07-18 15:29:09 +0200

    Link dotfiles from bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,4 +13,7 @@ fi
 echo "Homebrew-pakketten installeren..."
 brew bundle install --file="$DOTFILES_DIR/Brewfile"
 
-echo "Homebrew-pakketten zijn geïnstalleerd."
+echo "Dotfiles koppelen..."
+stow --dir="$DOTFILES_DIR" --target="$HOME" --verbose home
+
+echo "Setup voltooid."
