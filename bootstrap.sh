26442c8974b41db63a32aa6798b80696c03feee9
Nico van der Linden <nicolinden@gmail.com>
2026-07-18 16:39:49 +0200

    Add minimal Oh My Zsh setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,15 @@ fi
 echo "Homebrew-pakketten installeren..."
 brew bundle install --file="$DOTFILES_DIR/Brewfile"
 
+OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
+
+if [[ ! -d "$OH_MY_ZSH_DIR" ]]; then
+  echo "Oh My Zsh installeren..."
+  git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git "$OH_MY_ZSH_DIR"
+else
+  echo "Oh My Zsh is al geïnstalleerd."
+fi
+
 echo "Dotfiles koppelen..."
 stow --dir="$DOTFILES_DIR" --target="$HOME" --verbose home
 
--- home/.zshrc
+++ home/.zshrc
@@ -1,7 +1,13 @@
 PATH="$PATH:/Applications/WezTerm.app/Contents/MacOS"
 export PATH
 
-# prompt
+# Oh My Zsh
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME=""
+plugins=(git)
+source "$ZSH/oh-my-zsh.sh"
+
+# Starship prompt
 if command -v starship >/dev/null 2>&1; then
 	eval "$(starship init zsh)"
 fi
